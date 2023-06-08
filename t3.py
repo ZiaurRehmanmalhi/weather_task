767ecfbf58ac0194f3c53ce883063f9059818486
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-06-08 12:40:54 +0500

    add_colors

--- t3.py
+++ t3.py
@@ -4,6 +4,10 @@ from constent import MappingIndex
 
 file_values = read_files()
 
+red_code = '\033[91m'
+blue_code = '\033[34m'
+reset_code = '\033[0m'
+
 for file_value in file_values:
     date = file_value[MappingIndex.date]
     sliced_date = date.split('-')
@@ -11,6 +15,6 @@ for file_value in file_values:
     minimum_temperature = file_value[MappingIndex.minimum_temperature]
 
     if maximum_temperature and minimum_temperature is not None:
-        print(f"{sliced_date}{'+' * int(maximum_temperature)} {maximum_temperature}C")
-        print(f"{sliced_date}{'+' * int(minimum_temperature)} {minimum_temperature}C")
+        print(f"{sliced_date} {red_code} {'+' * int(maximum_temperature)} {reset_code}{maximum_temperature}C")
+        print(f"{sliced_date} {blue_code} {'+' * int(minimum_temperature)} {reset_code} {minimum_temperature}C")
 
